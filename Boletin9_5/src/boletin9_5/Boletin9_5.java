0bc7cddf8a7e83bf8696e1338cd823588fc2df80
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2015-11-17 09:17:37 +0100

    Boletin 9

---
+++ Boletin9_5/src/boletin9_5/Boletin9_5.java
@@ -0,0 +1,10 @@
+package boletin9_5;
+
+public class Boletin9_5 {
+
+    public static void main(String[] args) {
+        Clase9_5 opc=new Clase9_5();
+        opc.serie1();
+    }
+    
+}
---
+++ Boletin9_5/src/boletin9_5/Clase9_5.java
@@ -0,0 +1,28 @@
+package boletin9_5;
+
+import javax.swing.JOptionPane;
+
+public class Clase9_5 {
+    String serie="";
+    public int numElem(){
+        return Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de elementos de los que quieres que conste la serie "));
+    }
+    public void serie1(){
+        int num=numElem();
+        serie="";
+        for(int i=2;i<=num*2;i=i+2){
+            serie = serie + i +"+";
+            
+        }
+                JOptionPane.showMessageDialog(null, "La serie a) es = "+serie);
+        serie="";        
+        for(int i=1;i<=num;i++){
+            if (i%2!=0)
+                serie = serie+ "-" +i;
+            else serie= serie+ "+" +i;
+            
+        }        
+            JOptionPane.showMessageDialog(null, "la serie b) es = " + serie);
+    }
+    
+}
